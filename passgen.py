42a61a565443c9bc62aaa4c2d92cff1ea4610652
Maximus0898 <62480280+Maximus0898@users.noreply.github.com>
2020-07-03 23:18:25 +0500

    Initial commit

---
+++ passgen.py
@@ -0,0 +1,16 @@
+import random
+
+s = 'POIUYTREWQLKJHGFDSAMNBVCXZqwertyuiop[]asdfghjkl;zxcvbnm,./1234567890!@#$%^&*()_-=+'
+
+command = ""
+print("commands : generate, quit")
+while True:
+    command = input('>')
+    if command.lower() == 'generate':
+        passlen = int(input('Password length: '))
+        password = "".join(random.sample(s,passlen))
+        print(password)
+    elif command.lower() == 'quit':
+        break
+    else:
+        print("Sorry I don't understand that")
